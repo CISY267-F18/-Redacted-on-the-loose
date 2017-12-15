5b6a16242cf2c5c267e6f530c1eb7f8fa08073e1
Oates <G00260698@stu.raritanval.edu>
2017-12-15 14:44:18 -0500

    GodCards (not implemented)

---
+++ Assets/__Scripts/GodCards.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodCards
+{
+    /*switch(GodCard)
+    {
+        case Aphrodite:
+            
+            break;
+
+        case Apollo:
+                getZeus();
+            break;
+
+        case Ares:
+
+                getZeus();
+            break;
+
+        case Artemis:
+                getZeus();
+            break;
+
+        case Athena:
+                currPlayer = currPlayer + 2;
+                return(currPlayer);
+            break;
+
+        case Hera:
+
+                getZeus();
+            break;
+
+        case Hermes:
+                
+            break;
+
+        case Poseidon:
+                olympusCount = olympusCount -10;
+                return(olympusCount);
+                getZeus();
+            break;
+    }
+    public string getZeus()
+    {
+    return null;
+    }
+
+*/
+}
